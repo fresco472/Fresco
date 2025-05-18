429471f0f6bf5069dc7b35d74ca5dea739bd1f07
Your Name <you@example.com>
2025-05-18 17:17:50 +0300

    update

--- main.py
+++ main.py
@@ -1,17 +1,45 @@
 import telebot
 import keyboards
+import fsm
 
-BOT_TOKEN = '7602951193:AAEgttnkv2Dn80xHR7lBnrqL3t-0PvhHpWM' 
+BOT_TOKEN = '7602951193:AAEgttnkv2Dn80xHR7lBnrqL3t-0PvhHpWM'
+stater = fsm.FSM() 
 bot = telebot.TeleBot(BOT_TOKEN)
+
+def handler_def_state(message):
+    if message.text == "Фото 🖼":
+        stater.set_state(message.chat.id, fsm.IMAGE_STATE)
+        bot.send_message(message.chat.id, text='Напиши описание фото', reply_markup=keyboards.back)
+    elif message.text == "Текст 📝":
+        stater.set_state(message.chat.id, fsm.TEXT_STATE)
+        bot.send_message(message.chat.id, text='Напиши то, о чем ты хочешь у меня поинтересоваться', reply_markup=keyboards.back) 
+    else: 
+        bot.send_message(message.chat.id, text='Ниче не понял', reply_markup=keyboards.start) 
+
+def handler_image_state(message):
+    if message.text == "◀️ В меню":
+        stater.set_state(message.chat.id, fsm.DEFAULT_STATE)
+        bot.send_message(message.chat.id, text='Главное меню', reply_markup=keyboards.start)
+    else: 
+        bot.send_message(message.chat.id, text='Начинаю генерировать фото')
+
+def handler_text_state(message):  
+    if message.text == "◀️ В меню":
+            stater.set_state(message.chat.id, fsm.DEFAULT_STATE)
+            bot.send_message(message.chat.id, text='Главное меню', reply_markup=keyboards.start)
+    else: 
+            bot.send_message(message.chat.id, text='Начинаю генерировать текст')          
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     text = message.text
-    if text == "Фото 🖼":
-        bot.send_message(message.chat.id, "Напиши описание для фото", reply_markup=keyboards.back)
-    elif text == "Текст 📝":
-        bot.send_message(message.chat.id, "Начни общатся со мной в чате", reply_markup=keyboards.back)
-    else:
-        bot.send_message(message.chat.id, "Главное меню:", reply_markup=keyboards.start)
+    state = stater.get_state(message.chat.id)
+
+    if state == fsm.DEFAULT_STATE :
+        handler_def_state(message)
+    elif state == fsm.IMAGE_STATE :
+        handler_image_state(message)
+    elif state == fsm.TEXT_STATE :
+        handler_text_state(message)
 
 bot.polling()
